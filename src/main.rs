2db6eef0d2bd5d9ef98fbe776ff8caaefac129e9
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-04-13 07:46:15 +0200

    started matching

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,35 @@
 
 mod fa;
 mod nfa;
+
 use nfa::post2nfa;
+use fa::State;
+use fa::RState::Matching;
+
+struct List {
+    s: Vec<State>,
+    n: i32,
+}
+
+impl List {
+    pub fn new(s: Vec<State>) -> Self {
+        List { s, n: 0 }
+    }
+
+    pub fn is_match(&self) -> bool {
+        self.s
+            .iter()
+            .filter(|State(x)| x.borrow().clone() == Matching)
+            .count() > 0
+    }
+}
+
+fn fa_match(start: &State, s: String) -> bool {
+    let l1: List;
+    let l2: List;
+
+    true
+}
 
 fn main() {
     // let re = "abb.+.a.".to_owned();
@@ -17,5 +45,7 @@ fn main() {
     // let re = "a?".to_owned();
     // let re = "a+".to_owned();
     let bla = post2nfa(re);
-    // println!("{:?}", bla);
+    println!("{:#?}", bla);
+
+    // Simulating the NFA
 }
